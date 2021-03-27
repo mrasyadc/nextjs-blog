6ba4fadaf531e781a274cdd4a0e023c1d209e00e
rasyad-termux <mrcrasyad26@gmail.com>
2021-03-27 22:23:49 +0700

    new

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ export default function Home({allPostsData}) {
           <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <p>Hi!, I'm M Rasyad C. I'm a front end engineer at Google. You can contact me on <a href="https://twitter.com/mrcrasyad26">Twitter.</a></p>
+          <p>Hi!, I'm M Rasyad C. I'm a front end engineer at Google. You can reach me on <a href="https://twitter.com/mrcrasyad26">Twitter.</a></p>
           <p>
             (This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
